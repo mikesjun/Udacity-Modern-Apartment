2f9a8e63b1c58e6835a71dd9048e7ccfb9ed588a
mikesjun <mikesjun@gmail.com>
2017-08-10 03:19:58 -0700

    Initial commit

---
+++ Assets/VRND-Course2-Project/Scripts/HoursHand.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoursHand : MonoBehaviour {
+
+	float speed = 10f;
+
+	void Update ()
+	{
+		transform.Rotate(Vector2.up,speed * Time.deltaTime);
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Udacity-Modern-Apartment
